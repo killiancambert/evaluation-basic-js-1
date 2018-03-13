147c871d2b0b419ff0be78a8bdb6f1b29f1720da
killian <jclabaule@hotmail.fr>
2018-03-13 10:26:28 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,5 +1,14 @@
 var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+  
+  var input = [34, -5, 9, 18, 0, -7, 32]
+  var output = input.filter(values => values >= 0)
+  
+  // output = [] --> 1
+
+  /* input = []
+     output = [] --> 2 */
+  
+  return output
 }
 
 module.exports = {
