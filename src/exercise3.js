cca88eea7a031e2a5de602b555a7c15c678dfad6
killian <jclabaule@hotmail.fr>
2018-03-13 10:41:03 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,5 +1,21 @@
 var applyMap = function() {
-  // Your future job begins here ...
+  
+	// var output = [] --> 1
+
+	// var inputValues = [5, 7, 18]
+	// var output = [] --> 2
+
+	
+
+
+
+  var inputValues = [12, 26, 42]
+  function map(value) {
+      return value - 2
+    }
+    var inputMaps = [map]
+    var output = [map(inputValues)]
+  return output
 }
 
 module.exports = {
